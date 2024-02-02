9a5f06a99438eb0858b9b7502cbe92086b8f7cb0
RyanJStories <124539761+RyanJStories@users.noreply.github.com>
2024-02-01 19:17:44 -0600

    Completed Activity 1

--- root/blog/gatsby-config.js
+++ root/blog/gatsby-config.js
@@ -9,10 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby blog`,
+    description: ``,
+    author: `Ryan Scott`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    contact:{
+      name: 'Ryan Scott',
+      company: 'Blogs Inc.',
+      address: 'PO Box 1234'
+  }
   },
   plugins: [
     `gatsby-plugin-image`,
