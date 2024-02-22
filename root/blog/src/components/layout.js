77981397e39bf358abdb032c3b7884a47460b11d
RyanJStories <124539761+RyanJStories@users.noreply.github.com>
2024-02-22 17:22:38 -0600

    secondarily committed

--- root/blog/src/components/layout.js
+++ root/blog/src/components/layout.js
@@ -1,12 +1,7 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
- */
-
+import styled from "styled=components"
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import PropTypes from "prop-types"
 
 import Header from "./header"
 import "./layout.css"
@@ -22,30 +17,39 @@ const Layout = ({ children }) => {
     }
   `)
 
+const Content = styled.div`
+margin: 0 auto;
+max-width: var(--size-content);
+padding: var(--size-gutter);
+`
+
+
+
+
+
+
+
   return (
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+     <Content>
+      <main>{children}</main>
+      <footer
+      style={{
+        marginTop: `var(--space-5)`,
+        fontSize: `var(--font-sm)`,
+      }}>
+        {new Date().getFullYear()} &middot; Built with
+        {` `}
+        <a href="https://www.gatsbyjs.com"> Gatsby</a>
+      </footer>
+     </Content>
     </>
   )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired
 }
 
 export default Layout
